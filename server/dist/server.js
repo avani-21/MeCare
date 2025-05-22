d9d995f5785d2c52d0321cecfbe0589bc0e765fb
Avani-github <avanipoonthottam@gmail.com>
2025-05-22 17:10:06 +0530

    nodemon changed

--- server/dist/server.js
+++ server/dist/server.js
@@ -41,7 +41,10 @@ app.use((req, res, next) => {
 });
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000',
+        "https://mecare-ecru.vercel.app/",
+        "https://mecare.zapto.org"
+    ],
     credentials: true,
 }));
 app.use(morgen_1.default);
