d4ee5e9259d14b19563386ad5b0bb69cb2280bcf
Avani-github <avanipoonthottam@gmail.com>
2025-05-23 19:05:13 +0530

    server update

--- server/src/server.ts
+++ server/src/server.ts
@@ -36,7 +36,7 @@ app.use(cookieParser());
 
 app.use(cors({
     origin:[ 'http://localhost:3000',
-      "https://mecare-ecru.vercel.app/",
+      "https://mecare-ecru.vercel.app",
       "https://mecare.zapto.org"
     ],
     credentials: true,
